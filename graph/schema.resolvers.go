aa0e7d639cadcce16b8996ec486e0405e9be19af
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-24 13:06:24 +0200

    update UpsertCharacter Resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,11 +8,33 @@ import (
 	"cat-turner/outerbanks-api/graph/model"
 	"context"
 	"fmt"
+	"strconv"
 )
 
 // UpsertCharacter is the resolver for the upsertCharacter field.
 func (r *mutationResolver) UpsertCharacter(ctx context.Context, input model.CharacterInput) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented: UpsertCharacter - upsertCharacter"))
+	id := input.ID
+	var character model.Character
+	character.Name = input.Name
+	
+	n := len(r.Resolver.CharacterStore)
+	if n == 0 {
+		r.Resolver.CharacterStore = make(map[string]model.Character)
+	}
+	
+	if id != nil {
+		_, ok:= r.Resolver.CharacterStore[*id]
+		 if !ok {
+			  return nil, fmt.Errorf("not found")
+		}
+		r.Resolver.CharacterStore[*id] = character
+	} else {
+		//generate unique id
+		nid := strconv.Itoa(n+1)
+		character.ID = nid
+		r.Resolver.CharacterStore[nid] = character
+	}
+	return &character, nil
 }
 
 // Character is the resolver for the character field.
